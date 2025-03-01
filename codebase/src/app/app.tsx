6fa7694d7ee4fd701a20de9819eb6e2162681202
sushmitsarmah <sushmitsarmah@gmail.com>
2025-03-01 08:53:20 -0700

    authentication

--- codebase/src/app/app.tsx
+++ codebase/src/app/app.tsx
@@ -2,9 +2,18 @@ import Navbar from '@/components/custom/Navbar';
 // import NxWelcome from './nx-welcome';
 import Homepage from '@/pages/Home';
 import { Toaster } from "@/components/ui/sonner"
+import { usePrivy } from '@privy-io/react-auth';
+import { useEffect } from 'react';
 
 
 export function App() {
+    const { login, authenticated } = usePrivy();
+
+    useEffect(() => {
+      if (!authenticated) {
+        login();
+      }
+    }, [authenticated]);
   
   return (
     <div>
